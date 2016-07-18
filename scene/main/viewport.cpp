b15b64785eb4c87b7be84e3b952efb7677a984ba
Jaroslaw Filiochowski <jarfil@gmail.com>
2016-07-18 21:28:28 +0200

    Don't destroy and show again the same tooltip

--- scene/main/viewport.cpp
+++ scene/main/viewport.cpp
@@ -1525,6 +1525,10 @@ void Viewport::_gui_show_tooltip() {
 		return; // bye
 
 	if (gui.tooltip_popup) {
+		if (tooltip == gui.tooltip_label->get_text()) {
+			return; // don't destroy and show again the same tooltip
+		}
+
 		memdelete(gui.tooltip_popup);
 		gui.tooltip_popup=NULL;
 	}
